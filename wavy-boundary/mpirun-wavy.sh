46652533f6f69ac07c9ac162012634baa944b10e
Anton-Oleinik <anton.oleinik@warwick.ac.uk>
2026-08-19 11:57:11 +0100

    Added example output

--- wavy-boundary/mpirun-wavy.sh
+++ wavy-boundary/mpirun-wavy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Compiling..."
-CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 wavy.c -o wavy-mpi -lm -L$BASILISK/gl -lglutils -lfb_tiny
+CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 wavy-mpi.c -o wavy-mpi -lm -L$BASILISK/gl -lglutils -lfb_tiny
 
 if [[ "$?" -ne 0 ]]; then
     echo "Compilation error"
